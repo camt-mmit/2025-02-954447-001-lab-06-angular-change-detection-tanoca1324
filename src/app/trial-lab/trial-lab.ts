3adda2e5e4635748a84097238a5c3ef35457fc86
tanoca1324 <tanoca1324@gmail.com>
2026-01-05 13:55:28 +0700

    gg

--- src/app/trial-lab/trial-lab.ts
+++ src/app/trial-lab/trial-lab.ts
@@ -1,35 +1,25 @@
 import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-trial-lab',
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule ],
   templateUrl: './trial-lab.html',
   styleUrls: ['./trial-lab.scss'],
-  changeDetection:ChangeDetectionStrategy.OnPush,
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class TrialLab  implements OnInit {
   sections: any[] = [];
 
   ngOnInit() {
-    // เมื่อโหลดหน้าจอ ให้สร้าง Section แรกขึ้นมาทันที
-    this.addInitialData();
-  }
-
-  addInitialData() {
-    // สร้าง 1 Section ที่มี 1 Input เป็นค่าเริ่มต้น
-    this.sections.push({
-      id: Date.now(),
-      inputs: [{ value: 0 }]
-    });
+    this.addSection();
   }
 
   addSection() {
     this.sections.push({
       id: Date.now(),
-      inputs: [{ value: 0 }] // เมื่อเพิ่ม Section ใหม่ ให้มี 1 Input
+      inputs: [{ value: 0 }]
     });
   }
 
@@ -49,9 +39,15 @@ export class TrialLab  implements OnInit {
     }
   }
 
+
+  updateValue(sIdx: number, iIdx: number, event: Event) {
+    const inputElement = event.target as HTMLInputElement;
+    this.sections[sIdx].inputs[iIdx].value = Number(inputElement.value) || 0;
+  }
+
   getSectionTotal(sectionIndex: number): number {
     return this.sections[sectionIndex].inputs.reduce(
-      (acc: number, curr: any) => acc + (Number(curr.value) || 0), 0
+      (acc: number, curr: any) => acc + curr.value, 0
     );
   }
 }
